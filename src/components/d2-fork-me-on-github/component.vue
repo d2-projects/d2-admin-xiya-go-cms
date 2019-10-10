1be3a4bd005691a275655939ced5ed686f362345
FairyEver <1711467488@qq.com>
2019-10-10 21:54:28 +0800

    update index and auto install componnets

---
+++ src/components/d2-fork-me-on-github/component.vue
@@ -0,0 +1,15 @@
+<template>
+  <a target="blank" href="https://github.com/d2-projects/d2-admin-cms-go">
+    <!-- image from https://github.com/d2-projects/d2-ribbons -->
+    <img
+      style="position: absolute; top: 0; right: 0; border: 0; width: 150px;"
+      src="./image/darkblue@2x.png"
+      alt="Fork me on GitHub">
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'd2-fork-me-on-github'
+}
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,10 @@ import d2Container from './d2-container'
 
 // 注意 有些组件使用异步加载会有影响
 Vue.component('d2-container', d2Container)
-Vue.component('d2-icon', () => import('./d2-icon'))
-Vue.component('d2-icon-svg', () => import('./d2-icon-svg/index.vue'))
+
+const files = require.context('./', true, /component\.vue$/)
+
+files.keys().forEach(key => {
+  const component = files(key).default
+  Vue.component(component.name, component)
+})
