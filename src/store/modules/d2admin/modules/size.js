e1e04b72106a592be369b379643dcb3724c0b480
FairyEver <1711467488@qq.com>
2019-10-14 10:25:08 +0800

    简化 size

--- src/store/modules/d2admin/modules/size.js
+++ src/store/modules/d2admin/modules/size.js
@@ -39,41 +39,33 @@ export default context => ({
      * @param {Object} vuex context
      * @param {String} size 尺寸
      */
-    set ({ state, dispatch }, size) {
-      return new Promise(async resolve => {
-        // store 赋值
-        state.value = size
-        // 应用
-        dispatch('apply', true)
-        // 持久化
-        await dispatch('d2admin/db/set', {
-          dbName: 'sys',
-          path: 'size.value',
-          value: state.value,
-          user: true
-        }, { root: true })
-        // end
-        resolve()
-      })
+    async set ({ state, dispatch }, size) {
+      // store 赋值
+      state.value = size
+      // 应用
+      dispatch('apply', true)
+      // 持久化
+      await dispatch('d2admin/db/set', {
+        dbName: 'sys',
+        path: 'size.value',
+        value: state.value,
+        user: true
+      }, { root: true })
     },
     /**
      * @description 从持久化数据读取尺寸设置
      * @param {Object} vuex context
      */
-    load ({ state, dispatch }) {
-      return new Promise(async resolve => {
-        // store 赋值
-        state.value = await dispatch('d2admin/db/get', {
-          dbName: 'sys',
-          path: 'size.value',
-          defaultValue: 'default',
-          user: true
-        }, { root: true })
-        // 应用
-        dispatch('apply')
-        // end
-        resolve()
-      })
+    async load ({ state, dispatch }) {
+      // store 赋值
+      state.value = await dispatch('d2admin/db/get', {
+        dbName: 'sys',
+        path: 'size.value',
+        defaultValue: 'default',
+        user: true
+      }, { root: true })
+      // 应用
+      dispatch('apply')
     }
   }
 })
