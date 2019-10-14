3f4409f40e5ced2f70941af26d395cc0aa5ca033
FairyEver <1711467488@qq.com>
2019-10-14 11:28:59 +0800

    layout header-base-url component

---
+++ src/layout/header-aside/components/header-base-url/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <d2-api-base-url-controller>
+    <el-tooltip effect="dark" content="切换环境" placement="bottom">
+      <el-button class="d2-mr btn-text can-hover" type="text">
+        <d2-icon name="wifi" style="font-size: 18px;"/>
+      </el-button>
+    </el-tooltip>
+  </d2-api-base-url-controller>
+</template>
--- src/layout/header-aside/layout.vue
+++ src/layout/header-aside/layout.vue
@@ -27,6 +27,7 @@
         <div class="d2-header-right" flex-box="0">
           <!-- 如果你只想在开发环境显示这个按钮请添加 v-if="$env === 'development'" -->
           <d2-header-search @click="handleSearchClick"/>
+          <d2-header-base-url/>
           <d2-header-log/>
           <d2-header-fullscreen/>
           <d2-header-theme/>
@@ -94,6 +95,7 @@ import d2HeaderTheme from './components/header-theme'
 import d2HeaderUser from './components/header-user'
 import d2HeaderLog from './components/header-log'
 import d2HeaderColor from './components/header-color'
+import d2HeaderBaseUrl from './components/header-base-url'
 import { mapState, mapGetters, mapActions } from 'vuex'
 import mixinSearch from './mixins/search'
 export default {
@@ -112,7 +114,8 @@ export default {
     d2HeaderTheme,
     d2HeaderUser,
     d2HeaderLog,
-    d2HeaderColor
+    d2HeaderColor,
+    d2HeaderBaseUrl
   },
   data () {
     return {
