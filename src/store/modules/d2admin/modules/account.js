c769d5d6ba60d31f3674b68a4031e9787b49d004
FairyEver <1711467488@qq.com>
2019-10-14 09:31:35 +0800

    简化 account 模块

--- src/store/modules/d2admin/modules/account.js
+++ src/store/modules/d2admin/modules/account.js
@@ -91,25 +91,21 @@ export default context => ({
      * @description 用户登录后从持久化数据加载一系列的设置
      * @param {Object} vuex context
      */
-    load ({ dispatch }) {
-      return new Promise(async resolve => {
-        // DB -> store 加载用户名
-        await dispatch('d2admin/user/load', null, { root: true })
-        // DB -> store 加载主题
-        await dispatch('d2admin/theme/load', null, { root: true })
-        // DB -> store 加载页面过渡效果设置
-        await dispatch('d2admin/transition/load', null, { root: true })
-        // DB -> store 持久化数据加载上次退出时的多页列表
-        await dispatch('d2admin/page/openedLoad', null, { root: true })
-        // DB -> store 持久化数据加载侧边栏折叠状态
-        await dispatch('d2admin/menu/asideCollapseLoad', null, { root: true })
-        // DB -> store 持久化数据加载全局尺寸
-        await dispatch('d2admin/size/load', null, { root: true })
-        // DB -> store 持久化数据加载颜色设置
-        await dispatch('d2admin/color/load', null, { root: true })
-        // end
-        resolve()
-      })
+    async load ({ dispatch }) {
+      // DB -> store 加载用户名
+      await dispatch('d2admin/user/load', null, { root: true })
+      // DB -> store 加载主题
+      await dispatch('d2admin/theme/load', null, { root: true })
+      // DB -> store 加载页面过渡效果设置
+      await dispatch('d2admin/transition/load', null, { root: true })
+      // DB -> store 持久化数据加载上次退出时的多页列表
+      await dispatch('d2admin/page/openedLoad', null, { root: true })
+      // DB -> store 持久化数据加载侧边栏折叠状态
+      await dispatch('d2admin/menu/asideCollapseLoad', null, { root: true })
+      // DB -> store 持久化数据加载全局尺寸
+      await dispatch('d2admin/size/load', null, { root: true })
+      // DB -> store 持久化数据加载颜色设置
+      await dispatch('d2admin/color/load', null, { root: true })
     }
   }
 })
