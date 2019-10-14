088d792d512ccc34d313876fda84401737a1e1f7
FairyEver <1711467488@qq.com>
2019-10-14 09:45:39 +0800

    简化 color 模块

--- src/store/modules/d2admin/modules/color.js
+++ src/store/modules/d2admin/modules/color.js
@@ -13,50 +13,42 @@ export default context => ({
      * @param {Object} vuex context
      * @param {String} color 尺寸
      */
-    set ({ state, dispatch, commit }, color) {
-      return new Promise(async resolve => {
-        // 记录上个值
-        const old = state.value
-        // store 赋值
-        state.value = color || process.env.VUE_APP_ELEMENT_COLOR
-        // 持久化
-        await dispatch('d2admin/db/set', {
-          dbName: 'sys',
-          path: 'color.value',
-          value: state.value,
-          user: true
-        }, { root: true })
-        // 应用
-        commit('apply', {
-          oldColor: old,
-          newColor: state.value
-        })
-        // end
-        resolve()
+    async set ({ state, dispatch, commit }, color) {
+      // 记录上个值
+      const old = state.value
+      // store 赋值
+      state.value = color || process.env.VUE_APP_ELEMENT_COLOR
+      // 持久化
+      await dispatch('d2admin/db/set', {
+        dbName: 'sys',
+        path: 'color.value',
+        value: state.value,
+        user: true
+      }, { root: true })
+      // 应用
+      commit('apply', {
+        oldColor: old,
+        newColor: state.value
       })
     },
     /**
      * @description 从持久化数据读取颜色设置
      * @param {Object} vuex context
      */
-    load ({ state, dispatch, commit }) {
-      return new Promise(async resolve => {
-        // 记录上个值
-        const old = state.value
-        // store 赋值
-        state.value = await dispatch('d2admin/db/get', {
-          dbName: 'sys',
-          path: 'color.value',
-          defaultValue: process.env.VUE_APP_ELEMENT_COLOR,
-          user: true
-        }, { root: true })
-        // 应用
-        commit('apply', {
-          oldColor: old,
-          newColor: state.value
-        })
-        // end
-        resolve()
+    async load ({ state, dispatch, commit }) {
+      // 记录上个值
+      const old = state.value
+      // store 赋值
+      state.value = await dispatch('d2admin/db/get', {
+        dbName: 'sys',
+        path: 'color.value',
+        defaultValue: process.env.VUE_APP_ELEMENT_COLOR,
+        user: true
+      }, { root: true })
+      // 应用
+      commit('apply', {
+        oldColor: old,
+        newColor: state.value
       })
     }
   },
