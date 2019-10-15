47751771b41a8e7737b2e19b98f5f0cd7ae962af
FairyEver <1711467488@qq.com>
2019-10-15 22:12:34 +0800

    更新注释

--- src/utils/modules/fn.js
+++ src/utils/modules/fn.js
@@ -1,5 +1,11 @@
 import { get } from 'lodash'
 
+/**
+ * @description 从一个数据上尝试选择一个有效值
+ * @param {Object} parent 被取值的数据
+ * @param {Array} paths 可供选择的取值路径
+ * @param {*} defaultValue 默认值
+ */
 export function getFromMulti (parent = {}, paths = [], defaultValue = '') {
   let result = defaultValue
   for (let pathIndex = 0; pathIndex < paths.length; pathIndex++) {
