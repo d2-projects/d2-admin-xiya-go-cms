d0ab7835e711e14875de5fe3387704bd67ed9b70
FairyEver <1711467488@qq.com>
2019-10-16 15:02:33 +0800

    store 相关的代码片段

---
+++ src/store/modules/d2admin/snippets/snippets.js
@@ -0,0 +1,50 @@
+exports.default = function () {
+	let result = {}
+
+	const scope = 'javascript,typescript,vue,vue-html'
+
+	const helpers = [
+		'mapState',
+		'mapGetters',
+		'mapMutations',
+		'mapActions'
+	]
+
+	const d2adminModules = [
+		'account',
+		'fullscreen',
+		'page',
+		'theme',
+		'api',
+		'gray',
+		'releases',
+		'transition',
+		'color',
+		'log',
+		'search',
+		'ua',
+		'db',
+		'menu',
+		'size',
+		'user'
+	]
+
+	// 在组件中导入某个模块
+	helpers.forEach(helper => {
+		d2adminModules.forEach(d2adminModule => {
+			const name = `${helper} d2:${d2adminModule}`
+			result[name] = {
+				scope,
+				prefix: name,
+				body: [
+					`...${helper}('d2admin/${d2adminModule}', [`,
+					`  '\$\{1:${helper.replace('map', '').toLowerCase()} name\}'`,
+					`])\$\{2:,\}`
+				],
+				description: `# D2Admin 状态管理 | store\n# 在组件中使用 ${helper} 导入 d2admin ${d2adminModule} 模块`
+			}
+		})
+	})
+
+	return result
+}
