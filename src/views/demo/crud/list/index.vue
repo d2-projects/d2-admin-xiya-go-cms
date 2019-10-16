33990ba5f374b5a86f52a488db2689a713c6f678
FairyEver <1711467488@qq.com>
2019-10-16 23:06:54 +0800

    d2-pagination  组件片段

--- src/views/demo/crud/list/index.vue
+++ src/views/demo/crud/list/index.vue
@@ -3,15 +3,10 @@
     <template slot="header">
       <el-button type="primary" size="mini">新建</el-button>
     </template>
-    <!-- 表格 -->
     <el-table :data="tableData" height="100%" size="mini" stripe>
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
       <el-table-column prop="name" label="姓名" width="180"></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
     </el-table>
-    <!-- 分页 -->
     <d2-pagination slot="footer" v-bind="pagination" @change="onPaginationChange"/>
   </d2-container>
 </template>
@@ -25,20 +20,26 @@ export default {
         size: 10,
         total: 10000
       },
-      tableData: [...Array(50)].map(e => ({
-        date: '2016-05-02',
-        name: '王小虎',
+      tableData: [...Array(10)].map(e => ({
+        name: 'FairyEver',
         address: '上海市普陀区金沙江路 1518 弄'
       }))
     }
   },
   created () {
-    this.getList()
+    this.getTableData()
   },
   methods: {
-    async getList () {
-      const listData = await this.$api.MENU_LIST()
-      console.log(listData)
+    getTableData () {
+      // 模拟请求数据
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve()
+        }, 3000)
+      })
+    },
+    onPaginationChange () {
+      this.getTableData()
     }
   }
 }
