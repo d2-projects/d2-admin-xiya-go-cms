ddcd8baff3e4e2a422c87c9b6547c63d29debf18
FairyEver <1711467488@qq.com>
2019-10-20 17:38:18 +0800

    简化 jsx 代码

--- src/views/d2-table/expand/page.vue
+++ src/views/d2-table/expand/page.vue
@@ -19,14 +19,13 @@ export default {
         columns: [
           {
             type: 'expand',
-            render: ({ row, $index }) => {
-              return <section>
+            render: ({ row, $index }) =>
+              <section>
                 <p>row.name = { row.name }</p>
                 <p>row.email = { row.email }</p>
                 <p>row.county = { row.county }</p>
                 <p>$index = { $index }</p>
               </section>
-            }
           },
           { prop: 'name', label: '姓名' },
           { prop: 'email', label: '邮箱' },
