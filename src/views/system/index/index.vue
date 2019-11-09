4eee23da584ef2c7b075a63f5b797f68223f5ee4
FairyEver <1711467488@qq.com>
2019-11-09 19:13:32 +0800

    还原首页

--- src/views/system/index/index.vue
+++ src/views/system/index/index.vue
@@ -1,39 +1,5 @@
 <template>
   <d2-container>
     <d2-fork-me-on-github/>
-    <el-row>
-      <el-col :span="6">
-        <p>{{ value1 }}</p>
-        <d2-tree v-bind="tree1" v-model="value1" :source="$api.MENU_ALL"/>
-      </el-col>
-      <el-col :span="6">
-        <p>{{ value2 }}</p>
-        <d2-tree v-bind="tree2" v-model="value2" :source="$api.MENU_ALL"/>
-      </el-col>
-      <el-col :span="6"></el-col>
-      <el-col :span="6"></el-col>
-    </el-row>
   </d2-container>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      value1: 81,
-      tree1: {
-        defaultExpandAll: true,
-        nodeKey: 'id',
-        keyLabel: 'menu_name'
-      },
-      value2: [ 86 ],
-      tree2: {
-        multiple: true,
-        defaultExpandAll: true,
-        nodeKey: 'id',
-        keyLabel: 'menu_name'
-      }
-    }
-  }
-}
-</script>
