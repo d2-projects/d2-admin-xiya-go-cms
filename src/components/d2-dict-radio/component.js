19801b346327a6eff45d3cd6485dec34f4fb539b
FairyEver <1711467488@qq.com>
2019-11-09 22:46:39 +0800

    d2-dict-radio button

--- src/components/d2-dict-radio/component.js
+++ src/components/d2-dict-radio/component.js
@@ -3,15 +3,19 @@ import dict from '@/mixins/component.dict'
 export default {
   name: 'd2-dict-radio',
   mixins: [ dict ],
+  props: {
+    button: { type: Boolean, default: false, required: false }
+  },
   render () {
+    const tagName = this.button ? 'el-radio-button' : 'el-radio'
     const component =
       <el-radio-group { ...{ attrs: this.attrs } } vModel={ this.currentValue } on-change={ this.onChange }>
         {
           this.options.map(
             item =>
-              <el-radio label={ item.value }>
+              <tagName label={ item.value }>
                 { item.label }
-              </el-radio>
+              </tagName>
           )
         }
       </el-radio-group>
--- src/views/management/dept/index.js
+++ src/views/management/dept/index.js
@@ -91,7 +91,7 @@ export default {
           prop: 'status',
           label: '状态',
           default: 0,
-          render: <d2-dict-select vModel={ this.search.form.model.status } name="status" style="width:100px;" all/>
+          render: <d2-dict-radio vModel={ this.search.form.model.status } name="status" button all/>
         }
       ]
     }
