7980aad0f3cdc7bf14266d4f776500dcc5353f28
FairyEver <1711467488@qq.com>
2019-11-10 19:12:20 +0800

    完善菜单

--- src/views/management/menu/form.js
+++ src/views/management/menu/form.js
@@ -13,8 +13,34 @@ export default {
   },
   computed: {
     setting () {
+      const url = {
+        prop: 'url',
+        default: '/',
+        label: '请求地址',
+        rule: { required: true, message: '必填', trigger: 'change' },
+        render: <el-input vModel={ this.form.model.url } clearable/>
+      }
+      const perms = {
+        prop: 'perms',
+        default: '',
+        label: '权限标识',
+        render: <el-input vModel={ this.form.model.perms } clearable/>
+      }
+      const icon = {
+        prop: 'icon',
+        default: '',
+        label: '图标',
+        render: <d2-icon-select vModel={ this.form.model.icon }/>
+      }
+      const visible = {
+        prop: 'visible',
+        default: 1,
+        label: '可见性',
+        rule: { required: true, message: '必填', trigger: 'change' },
+        render: <d2-dict-radio name="visible" vModel={ this.form.model.visible } button/>
+      }
       return [
-        // 基础设置
+        // 通用
         {
           prop: 'menu_name',
           default: '',
@@ -42,42 +68,21 @@ export default {
           rule: { required: true, message: '必填', trigger: 'change' },
           render: <el-input-number min={ 1 } vModel={ this.form.model.order_num }/>
         },
-        // 基础设置 end
-        // 菜单类型 menu_type === 目录 1：[菜单图标] [是否外链] [路由地址] [可见性]
-        // 菜单类型 menu_type === 菜单 2：[菜单图标] [是否外链] [路由地址] [组件路径] [权限标识] [可见性]
+        // 通用 end
+        // 菜单类型 menu_type === 目录 1：[菜单图标] [是否外链?] [路由地址] [可见性]
+        ...this.form.model.menu_type === 1 ? [ icon, url, visible ] : [],
+        // 菜单类型 menu_type === 菜单 2：[菜单图标] [是否外链?] [路由地址] [组件路径?] [权限标识] [可见性]
+        ...this.form.model.menu_type === 2 ? [ icon, url, perms, visible ] : [],
         // 菜单类型 menu_type === 按钮 3：[权限标识]
-        {
-          prop: 'url',
-          default: '/',
-          label: '请求地址',
-          rule: { required: true, message: '必填', trigger: 'change' },
-          render: <el-input vModel={ this.form.model.url } clearable/>
-        },
-        {
-          prop: 'perms',
-          default: '',
-          label: '权限标识',
-          render: <el-input vModel={ this.form.model.perms } clearable/>
-        },
-        {
-          prop: 'icon',
-          default: '',
-          label: '图标',
-          render: <d2-icon-select vModel={ this.form.model.icon }/>
-        },
-        {
-          prop: 'visible',
-          default: 1,
-          label: '可见性',
-          rule: { required: true, message: '必填', trigger: 'change' },
-          render: <d2-dict-radio name="visible" vModel={ this.form.model.visible } button/>
-        },
+        ...this.form.model.menu_type === 3 ? [ perms ] : [],
+        // 通用
         {
           prop: 'remark',
           default: '',
           label: '备注',
           render: <el-input vModel={ this.form.model.remark } clearable/>
         }
+        // 通用 end
       ]
     }
   }
