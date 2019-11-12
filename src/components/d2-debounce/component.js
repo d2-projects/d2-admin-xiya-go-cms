6eb68535a2f8965b370cd131696ec12f85307b66
FairyEver <1711467488@qq.com>
2019-11-12 19:16:19 +0800

    d2-debounce + d2-throttle

---
+++ src/components/d2-debounce/component.js
@@ -0,0 +1,32 @@
+// https://www.lodashjs.com/docs/latest#_debouncefunc-wait0-options
+
+export default {
+  name: 'd2-debounce',
+  functional: true,
+  render (createElement, context) {
+    // [options.leading=false] (boolean): 指定在延迟开始前调用
+    // [options.maxWait] (number): 设置 func 允许被延迟的最大值
+    // [options.trailing=true] (boolean): 指定在延迟结束后调用
+    const options = ({ leading = false, maxWait, trailing = true } = {}) => ({ leading, maxWait, trailing })(context.props)
+    const wait = context.props.wait
+    const vnodeList = context.slots().default
+    if (vnodeList === undefined) {
+      console.warn('<d2-debounce> 组件必须要有子元素')
+      return null
+    }
+    const vnode = vnodeList[0] || null
+    if (vnode.tag === 'button') {
+      const defaultFun = vnode.data.on.click
+      const debounceFun = this.$_.debounce(defaultFun, wait, options)
+      vnode.data.on.click = debounceFun
+    } else if (vnode.componentOptions && vnode.componentOptions.tag === 'el-button') {
+      const defaultFun = vnode.componentOptions.listeners.click
+      const debounceFun = this.$_.debounce(defaultFun, wait, options)
+      vnode.componentOptions.listeners.click = debounceFun
+    } else {
+      console.warn('<d2-debounce> 组件内只能出现下面组件的任意一个且唯一 el-button、button')
+      return vnode
+    }
+    return vnode
+  }
+}
---
+++ src/components/d2-throttle/component.js
@@ -0,0 +1,29 @@
+export default {
+  name: 'd2-throttle',
+  functional: true,
+  render (createElement, context) {
+    // [options.leading=true] (boolean): 指定调用在节流开始前
+    // [options.trailing=true] (boolean): 指定调用在节流结束后
+    const options = ({ leading = true, trailing = true } = {}) => ({ leading, trailing })(context.props)
+    const wait = context.props.wait
+    const vnodeList = context.slots().default
+    if (vnodeList === undefined) {
+      console.warn('<d2-throttle> 组件必须要有子元素')
+      return null
+    }
+    const vnode = vnodeList[0] || null
+    if (vnode.tag === 'button') {
+      const defaultFun = vnode.data.on.click
+      const throttleFun = this.$_.throttle(defaultFun, wait, options)
+      vnode.data.on.click = throttleFun
+    } else if (vnode.componentOptions && vnode.componentOptions.tag === 'el-button') {
+      const defaultFun = vnode.componentOptions.listeners.click
+      const throttleFun = this.$_.throttle(defaultFun, wait, options)
+      vnode.componentOptions.listeners.click = throttleFun
+    } else {
+      console.warn('<d2-throttle> 组件内只能出现下面组件的任意一个且唯一 el-button、button')
+      return vnode
+    }
+    return vnode
+  }
+}
