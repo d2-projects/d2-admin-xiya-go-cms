c9254d16cbcaac281c4e3fdcf3ba6f19136397f9
FairyEver <1711467488@qq.com>
2019-12-23 22:00:50 +0800

    更新示例

--- src/views/system/index/index.vue
+++ src/views/system/index/index.vue
@@ -3,20 +3,26 @@
     <d2-fork-me-on-github/>
     <el-row>
       <el-col :span="12">
-        <p>需要某项权限</p>
-        <el-button v-permission="'system:user:remove'">v-permission="'system:user:remove'"</el-button>
-        <p>需要至少一项权限</p>
-        <el-button v-permission="['system:user:remove', 'system:user:edit']">v-permission="['system:user:remove', 'system:user:edit']"</el-button>
-        <p>需要列表中全部权限</p>
-        <el-button v-permission.all="['system:user:remove', 'system:user:edit']">v-permission.all="['system:user:remove', 'system:user:edit']"</el-button>
+        <p>需要具有某项权限</p>
+        <p>v-permission="'system:user:remove'"</p>
+        <el-button v-permission="'system:user:remove'">Button</el-button>
+        <p>需要具有至少一项权限</p>
+        <p>v-permission="['system:user:remove', 'system:user:edit']"</p>
+        <el-button v-permission="['system:user:remove', 'system:user:edit']">Button</el-button>
+        <p>需要具有给出的全部权限</p>
+        <p>v-permission.all="['system:user:remove', 'system:user:edit']"</p>
+        <el-button v-permission.all="['system:user:remove', 'system:user:edit']">Button</el-button>
       </el-col>
       <el-col :span="12">
-        <p>不允许某项权限</p>
-        <el-button v-permission.not="'system:user:remove'">v-permission.not="'system:user:remove'"</el-button>
-        <p>不允许其中任意一项权限</p>
-        <el-button v-permission.not="['system:user:remove', 'system:user:edit']">v-permission.not="['system:user:remove', 'system:user:edit']"</el-button>
-        <p>至少不包含列表中一项权限</p>
-        <el-button v-permission.not.all="['system:user:remove', 'system:user:edit']">v-permission.not.all="['system:user:remove', 'system:user:edit']"</el-button>
+        <p>不允许具有某项权限</p>
+        <p>v-permission.not="'system:user:remove'"</p>
+        <el-button v-permission.not="'system:user:remove'">Button</el-button>
+        <p>不允许具有其中任意一项权限</p>
+        <p>v-permission.not="['system:user:remove', 'system:user:edit']"</p>
+        <el-button v-permission.not="['system:user:remove', 'system:user:edit']">Button</el-button>
+        <p>至少不具有其中一项权限</p>
+        <p>v-permission.not.all="['system:user:remove', 'system:user:edit']"</p>
+        <el-button v-permission.not.all="['system:user:remove', 'system:user:edit']">Button</el-button>
       </el-col>
     </el-row>
   </d2-container>
